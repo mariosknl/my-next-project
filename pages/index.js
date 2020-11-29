af68b79bad41fbbd1b6affc7adbaec6adcb4b80b
mariosknl <mariosknl@outlook.com>
2020-11-29 10:11:42 +0200

    head added

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
+import Head from "next/head";
+
 function Home() {
-  return <div>Welcome, Explorer!</div>;
+  return (
+    <>
+      <Head>
+        <title>My Next.js Site!</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div>Welcome, Explorer!</div>
+    </>
+  );
 }
 
 export default Home;
