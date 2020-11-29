c413840ee5d07e9c8a61266fb903ff0ae3462cbf
mariosknl <mariosknl@outlook.com>
2020-11-29 10:43:48 +0200

    performance reportWebVital object created

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f6adac060c3b0a80ef94";
+/******/ 	var hotCurrentHash = "458c9c4754f646d2c5d2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
